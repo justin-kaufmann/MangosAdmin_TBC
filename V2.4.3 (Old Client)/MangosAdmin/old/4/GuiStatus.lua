5db58302ad67d3a458f1fd0119694f02bf7b80f8
Justin Kaufmann <86347766+justin-kaufmann@users.noreply.github.com>
2025-10-05 19:42:51 +0200

    Delete V2.4.3 (Old Client)/MangosAdmin/old directory

--- V2.4.3 (Old Client)/MangosAdmin/old/4/GuiStatus.lua
+++
@@ -1,9 +0,0 @@
--- ========= Status-Label (Fly: AN/AUS) =========
-MangosAdmin = MangosAdmin or {}
-
-function MangosAdmin.CreateStatusLabel(parent)
-  MangosAdmin_StatusLabel = parent:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-  MangosAdmin_StatusLabel:SetPoint("TOP", parent, "TOP", 0, -34)
-  MangosAdmin_StatusLabel:SetText("Fly: AUS")
-  return MangosAdmin_StatusLabel
-end
